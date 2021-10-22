d7d7962d8d625b65faff1bd404843b53dcaa3c99
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2021-10-22 20:58:30 -0300

    Add files via upload

---
+++ projSomaPOO/projSomaPOO/Soma.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projSomaPOO
+{
+    class Soma
+    {
+        #region atributos
+        private int num1;
+        private int num2;
+        private int resultado;
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(int n)
+        {
+            num1 = n;
+        }
+
+        public void setNum2(int n)
+        {
+            num2 = n;
+        }
+
+        public int getNum1()
+        {
+            return num1;
+        }
+
+        public int getNum2()
+        {
+            return num2;
+        }
+
+        public int getResultado()
+        {
+            return resultado;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            resultado = num1 + num2;
+        }
+        #endregion
+    }
+}
